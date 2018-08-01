d5b2afbdb38406f596689d04012e45c5d900a8b8
Lauren Perchitti <laurenperchitti@gmail.com>
2018-08-01 18:39:26 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export class FillerText extends React.Component {
+class FillerText extends React.Component {
   render() {
     <p>"I am a filler text. I can be used to fill your screen. Amazing!"</p>
   }
 }
+
+export default FillerText
